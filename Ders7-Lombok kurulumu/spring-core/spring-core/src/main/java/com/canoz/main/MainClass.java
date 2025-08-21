e8371b4af13be4a0379a8ff55671471c05b4f622
Ozyasar <canozyasar35@gmail.com>
2025-08-21 09:50:19 +0300

    bean oluşturma 2

--- Ders7-Lombok kurulumu/spring-core/spring-core/src/main/java/com/canoz/main/MainClass.java
+++ Ders7-Lombok kurulumu/spring-core/spring-core/src/main/java/com/canoz/main/MainClass.java
@@ -1,15 +1,31 @@
 package com.canoz.main;
 
+import java.util.Iterator;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.canoz.config.AppConfig;
+import com.canoz.model.User;
+import com.canoz.service.UserService;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		
 		
 		
-		new Ann
+	 ApplicationContext context= new AnnotationConfigApplicationContext(AppConfig.class);
+	 UserService userService= context.getBean(UserService.class);
+	 // config olarak işaretlenmiş classımızı ekliyoruz 
+		// içindeki beanları 
 		
-		//UserService userService2=new UserService(); 
-
+	 
+	 for (User user = userService.getUserList()) {
+		 System.out.println(user);
+		
+	}
 
 	}
 
